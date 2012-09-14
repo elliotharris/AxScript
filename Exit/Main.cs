38fe9698697d74eef8c2b16ac08815322463ceee
Elliot Harris <elliotharris@live.co.uk>
2012-09-14 21:24:41 +0100

    Changed the exit module's exit codes to be consts.

--- Exit/Main.cs
+++ Exit/Main.cs
@@ -5,15 +5,15 @@ namespace Exit
 {
     public class Main
     {
-        [ExportAx("EXITCODE_ERROR", "If the type given by the exit point matches this, then an error was thrown.")] public static int ExitCodeError = 0;
+        [ExportAx("EXITCODE_ERROR", "If the type given by the exit point matches this, then an error was thrown.")] public const int EXIT_CODE_ERROR = 0;
 
-        [ExportAx("EXITCODE_CLEAN", "If the type given by the exit point matches this, then the script finished execution as expected.")] public static int ExitCodeClean = 1;
+        [ExportAx("EXITCODE_CLEAN", "If the type given by the exit point matches this, then the script finished execution as expected.")] public const int EXIT_CODE_CLEAN = 1;
 
         [HookAx("ExitPoint", "Similarly to EntryPoint, this label defines the function to call when code exits.")]
         public static void InitExitHandler(AxInterpreter ax, AxFunction func)
         {
-            ax.ScriptError += exception => func.Call<Boolean>(ax, new object[] {ExitCodeError, exception});
-            ax.ScriptEnd += exception => func.Call<Boolean>(ax, new object[] { ExitCodeClean, exception });
+            ax.ScriptError += exception => func.Call<Boolean>(ax, new object[] {EXIT_CODE_ERROR, exception});
+            ax.ScriptEnd += exception => func.Call<Boolean>(ax, new object[] { EXIT_CODE_CLEAN, exception });
         }
     }
 }
